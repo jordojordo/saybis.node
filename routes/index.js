529f0fb063f24b12ff57c9ab299b8accb293f2ce
jordonleach@gmail.com <jordonleach@gmail.com>
2021-07-08 17:26:29 -0400

    https added to header

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { createTransaction } = require("../api/request");
 
 router.post("/", function (req, res) {
   createTransaction(req);
-  res.header("Access-Control-Allow-Origin", "saybis.xyz");
+  res.header("Access-Control-Allow-Origin", "https://saybis.xyz");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
